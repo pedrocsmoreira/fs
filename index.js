fc79d2e3268f47b67ef74a3c612fdd4344530635
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2023-08-28 11:25:55 +0100

    cors error correction

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('express')();
+const cors = require('cors');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 4000;
