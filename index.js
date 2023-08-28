f3464538d676f29b6205bed47c495362048ad2f9
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2023-08-28 11:15:44 +0100

    done

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 4000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-    res.render('index.html', {port: port});
+    const porta = process.env.PORT || 4000;
+    var conn = "".concat("ws://localhost:", porta);
+    res.render('home', {port: conn});
 });
 
 io.on('connection', (socket) => {
